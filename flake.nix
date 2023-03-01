4f1d7bb8478eb90fea72b869b4c3f0d89e179c9b
Peter Becich <peterbecich@gmail.com>
2023-02-28 19:25:16 -0800

    update sources

--- flake.nix
+++ flake.nix
@@ -1,15 +1,26 @@
 {
   # This is a template created by `hix init`
-  inputs.haskellNix.url = "github:input-output-hk/haskell.nix";
-  inputs.nixpkgs.follows = "haskellNix/nixpkgs-unstable";
-  inputs.flake-utils.url = "github:numtide/flake-utils";
-  outputs = { self, nixpkgs, flake-utils, haskellNix }:
+  inputs = {
+    hackageNix = {
+      url = "github:input-output-hk/hackage.nix";
+      flake = false;
+    };
+    nixpkgs.url = "github:peterbecich/nixpkgs/update-hackage";
+    haskellNix = {
+      url = "github:input-output-hk/haskell.nix";
+      inputs.hackage.follows = "hackageNix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    flake-utils.url = "github:numtide/flake-utils";
+
+  };
+  outputs = { self, nixpkgs, flake-utils, haskellNix, ... }:
     let
       supportedSystems = [
         "x86_64-linux"
-        "x86_64-darwin"
-        "aarch64-linux"
-        "aarch64-darwin"
+        # "x86_64-darwin"
+        # "aarch64-linux"
+        # "aarch64-darwin"
       ];
     in
       flake-utils.lib.eachSystem supportedSystems (system:
